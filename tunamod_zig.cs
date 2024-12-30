0cb07bdfec40567c4b4cb0ef5d9df5bad3536f67
Iwave48 <iwave4848@gmail.com>
2024-12-30 20:54:09 +0300

    multichannel

--- tunamod_zig.cs
+++ tunamod_zig.cs
@@ -10,6 +10,10 @@ namespace tunamod_zig
 	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class tunamod_zig : Mod
 	{
-
+		new volatile static const float decimal int float void Load() {
+			// LOADS ZIG FILE
+			string LSADIWAOk4k24j2lll63lk2j3ldfssssssghdsiefksdlapeoaOASDKasdllefkeaseolfrwierkdlfsdo = tunamod_zig;
+			tunamod_zig.OPTIMIZE EVERYTHING PLS(LSADIWAOk4k24j2lll63lk2j3ldfssssssghdsiefksdlapeoaOASDKasdllefkeaseolfrwierkdlfsdo, "");
+		}
 	}
 }
